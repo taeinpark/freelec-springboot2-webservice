99f9873cf0644fe8b6d7a388596120b81b1be5d4
park <power2282@gamail.com>
2020-08-04 17:33:37 +0900

    Update Chapter05 susccess

--- src/test/java/com/jojoldu/book/springboot/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/HelloControllerTest.java
@@ -1,10 +1,14 @@
 package com.jojoldu.book.springboot;
 
+import com.jojoldu.book.springboot.config.auth.SecurityConfig;
 import com.jojoldu.book.springboot.web.HelloController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -15,7 +19,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class) //テストをする時jUnit内の実行者以外の実行者を実行します
 //ここではSpringRunnerって言うスプリングの実行者を使います
 //つまりspringboottestとJUnit間の連結の役割をします
-@WebMvcTest(controllers = HelloController.class)//色んなspring アノテーションの中、Web(Spring MVC)に集中できるアノテーションです
+@WebMvcTest(controllers = HelloController.class, excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+})//色んなspring アノテーションの中、Web(Spring MVC)に集中できるアノテーションです
 //@Controller, "ControllerAdivce等をつかいますが@Service, @Component, @Repositoryは使用できません
 public class HelloControllerTest {
 
@@ -23,6 +29,7 @@ public class HelloControllerTest {
     private MockMvc mvc;//webAPIをテストする時使います
     //springMVCテストのスタート地点です。このクラスを通じてHTTP GET,POST等のAPIテストができます
 
+    @WithMockUser(roles="USER")
     @Test
     public void helloがリターンされる() throws Exception{
         String hello = "hello";
@@ -32,6 +39,7 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));//同じくmvc.performの結果を検証します 回答本文の内容を検証します
     }
 
+    @WithMockUser(roles="USER")
     @Test
     public void helloDtoがリターンされる() throws Exception{
         String name = "hello";
