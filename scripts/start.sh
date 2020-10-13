1d3faaa363fe3c4540c97fd0c119a44fe11e4e02
taeinpark <power2282@naver.com>
2020-10-13 18:09:58 +0900

    Chapter 10 properties

--- scripts/start.sh
+++ scripts/start.sh
@@ -10,7 +10,7 @@ PROJECT_NAME=freelec-springboot2-webservice
 echo ">Build 파일복사"
 echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
 
-cp $REPOSITORY/zip.jar $REPOSITORY/
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo ">새 애플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*jar | tail -n 1)
